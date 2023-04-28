09c8fb894afdedca0c56e6df069513be8451353c
Jean-Christophe <malaval@topdata.fr>
2023-04-28 23:00:00 +0200

    button in window

--- renamer/target/debug/deps/renamer.d
+++
@@ -1,5 +0,0 @@
-D:\www\perso\file_renamer\renamer\target\debug\deps\renamer.exe: src\main.rs
-
-D:\www\perso\file_renamer\renamer\target\debug\deps\renamer.d: src\main.rs
-
-src\main.rs:
--- renamer/target/debug/renamer.d
+++
@@ -1,1 +0,0 @@
-D:\www\perso\file_renamer\renamer\target\debug\renamer.exe: D:\www\perso\file_renamer\renamer\src\main.rs
